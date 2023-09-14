56fd65578160ee54dfbff61772bf1b4d5e68a56f
Emir Pirija <emir.pirija@outlook.com>
2023-09-14 15:34:57 +0200

    Add files via upload

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
+
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "48a4d912-9c95-4e3d-9531-b90bc5676d99",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "b0f1b45c-724f-4f76-8065-1a035c06a841" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+
+});
+
+app.listen(3001);
